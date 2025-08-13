54f509dda930ba3cb47d9d42f761c968cdb0a55f
aayush4jha <149305999+aayush4jha@users.noreply.github.com>
2025-08-13 11:17:10 +0530

    Time: 0 ms (100%), Space: 10 MB (43.02%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,31 +1,15 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        vector<char> ans;
-        for(int i=0;i<s.size();i++)
+        int left=0, right=s.length()-1;
+        while(left<right)
         {
-            int value = s[i];
-            if(value<=57 && value>=48)
+            if(!isalnum(s[left])) left++;
+            else if(!isalnum(s[right])) right--;
+            else if(tolower(s[left])!=tolower(s[right])) return false;
+            else
             {
-                ans.push_back(s[i]);
-                continue;
-            }
-            if(value<=122 && value>=97 || value<=90 && value>=65)
-            {
-                if(value<=90 && value>=65){
-                    char new_value = value+32;
-                    ans.push_back(new_value);
-                }
-                else{
-                    ans.push_back(value);
-                }
-            }
-        }
-        for(int i=0;i<ans.size();i++)
-        {
-            if(int(ans[i])!=int(ans[ans.size()-1-i]))
-            {
-                return false;
+                left++; right--;
             }
         }
         return true;
